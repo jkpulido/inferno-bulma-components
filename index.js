08d6811e363b3ec0f438aab04b4ecb2710e67f40
Juan K. Pulido <dxkernel@gmail.com>
2019-02-07 07:52:42 -0500

    avances navbar

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import "bulma/css/bulma.min.css"
 import "font-awesome/css/font-awesome.css";
 import Table from './components/table/Table';
 import GridList from './components/GridList';
+import Navbar from './components/Navbar';
 
 export {
-	Table, GridList
+	Table, GridList, Navbar
 }
